23c4d10fe39a709fef3d3b60e50435752462c970
shashank <shashank78680@gmail.com>
2024-01-21 19:18:16 +0530

    search movie working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+
+  const fetchMoviesData = useCallback(async ()=>{
+    const response = await fetch(`http://www.omdbapi.com/?apikey=b22b4728&s=${searchInput}`);
+    const movie = await response.json();
+    setMovies(movie.Search)
+  }, [searchInput]);
+
+  // on load
+  useEffect(()=>{
+    fetch("http://www.omdbapi.com/?apikey=b22b4728&s=avengers")
+    .then((data)=> data.json())
+    .then((data)=> setMovies(data.Search))
+  },[]);
+
   return (
     <>
       <div className="w-full h-20 text-white bg-slate-950 flex justify-center items-center font-bold text-5xl">
@@ -11,30 +28,22 @@ function App() {
         <input 
           className='w-[35%] h-14 bg-slate-300 outline-none border-none px-2 rounded-lg'
           type="text"
+          value={searchInput}
+          onChange={(e)=>setSearchInput(e.target.value)}
         />
         <button
           className='h-14 w-20 mx-2 bg-cyan-700 hover:bg-cyan-800 px-1 py-1 rounded-lg font-bold text-white'
+          onClick={()=>fetchMoviesData()}
         >
           Search
         </button>
       </div>
       <div className='grid grid-cols-4 justify-between'>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
+        {
+          movies.map((movieData)=>(
+            <MovieCard name={movieData.Title} imageUrl={movieData.Poster}/>
+          ))
+        }
       </div>
       <div className='bg-red-50 w-12 flex justify-between my-12 mx-auto'>
         <img src="src\images\prev-btn.png"></img>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-function MovieCard() {
+function MovieCard({name, imageUrl}) {
     return (
-        <div className="w-72 bg-slate-200 flex flex-col justify-center items-center mx-auto my-2 rounded-lg">
+        <div 
+            className="w-72 bg-slate-200 flex flex-col justify-center items-center mx-auto my-2 rounded-lg"
+        >
             <img 
-                src="src\images\movie_unavailable_image.png" 
+                src={imageUrl}
                 alt="Movie Image" 
                 className="w-fit my-2 px-2"
+                // onClick={()=>alert("Clicked")}
             >
             </img>
-            <p className="my-2 text-lg font-bold">Movie Name</p>
+            <p className="my-2 text-lg font-bold">{name}</p>
         </div>
     );
 }
